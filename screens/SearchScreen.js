78c1528b38686f02977b849e4707a57ddce44a45
payelbera <60781680+payelbera@users.noreply.github.com>
2020-12-02 00:37:40 +0530

    Add files via upload

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,48 +1,97 @@
 import React from 'react';
-import { StyleSheet, Text, View,ScrollView } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import { Text, View, FlatList, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import db from '../config'
-export default class SearchScreen extends  React.Component{
+import { ScrollView } from 'react-native-gesture-handler';
 
-constructor(){
-    super()
-    this.state={
-        allTransactions:[],
-        lastVisibleTransaction:null
+
+
+export default class Searchscreen extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        allTransactions: [],
+        lastVisibleTransaction: null,
+        search:''
+      }
     }
-}
 
-componentDidMount = async ()=>{
-  const query = await db.collection("transaction").limit(12).get()
-  query.docs.map((doc)=>{
-    console.log(doc.data())
-    this.setState({
-      //allTransactions:[...this.state.allTransactions,doc.data()],
-      allTransactions:[],
-      lastVisibleTransaction: doc
-    })
-    console.log("hh ")
-  })
-}
+    fetchMoreTransactions = async ()=>{
+      var text = this.state.search.toUpperCase()
+      var enteredText = text.split("")
 
-fetchMoreTransactions=async()=>{
-  console.log("fetch more txn")
-const query=  await db.collection('transaction').startAfter(this.state.lastVisibleTransaction).limit(12).get()
-console.log("len "+query.docs.length)
-console.log(doc.data())
-query.docs.map(()=>{
-this.setState({
-    allTransactions:[...this.state.allTransactions,doc.data()],
-    lastVisibleTransaction:doc
-})
-console.log("fetch more ")
-})
+      
+      if (enteredText[0].toUpperCase() ==='B'){
+      const query = await db.collection("transaction").where('bookId','==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+      query.docs.map((doc)=>{
+        this.setState({
+          allTransactions: [...this.state.allTransactions, doc.data()],
+          lastVisibleTransaction: doc
+        })
+      })
+    }
+      else if(enteredText[0].toUpperCase() === 'S'){
+        const query = await db.collection("transaction").where('bookId','==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+        query.docs.map((doc)=>{
+          this.setState({
+            allTransactions: [...this.state.allTransactions, doc.data()],
+            lastVisibleTransaction: doc
+          })
+        })
+      }
+  }
 
-}
-    render(){
-        return (
-        
-<FlatList
+    searchTransactions= async(text) =>{
+      console.log(text)
+      var enteredText = text.split("")  
+      console.log(enteredText)
+      if (enteredText[0].toUpperCase() ==='B'){
+        console.log("B")
+        const transaction =  await db.collection("transaction").where('bookId','==',text).get()
+        transaction.docs.map((doc)=>{
+          this.setState({
+            allTransactions:[...this.state.allTransactions,doc.data()],
+            lastVisibleTransaction: doc
+          })
+        })
+      }
+      else if(enteredText[0].toUpperCase() === 'S'){
+        console.log("S")
+
+        const transaction = await dS.collection('transaction').where('studentId','==',text).get()
+        transaction.docs.map((doc)=>{
+          this.setState({
+            allTransactions:[...this.state.allTransactions,doc.data()],
+            lastVisibleTransaction: doc
+          })
+        })
+      }
+    }
+
+    componentDidMount = async ()=>{
+      const query = await db.collection("transaction").limit(10).get()
+      query.docs.map((doc)=>{
+        this.setState({
+          allTransactions: [],
+          lastVisibleTransaction: doc
+        })
+      })
+    }
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.searchBar}>
+        <TextInput 
+          style ={styles.bar}
+          placeholder = "Enter Book Id or Student Id"
+          onChangeText={(text)=>{this.setState({search:text})}}/>
+          <TouchableOpacity
+            style = {styles.searchButton}
+            onPress={()=>{this.searchTransactions(this.state.search)}}
+          >
+            <Text>Search</Text>
+          </TouchableOpacity>
+          </View>
+        <FlatList
           data={this.state.allTransactions}
           renderItem={({item})=>(
             <View style={{borderBottomWidth: 2}}>
@@ -54,9 +103,40 @@ console.log("fetch more ")
           )}
           keyExtractor= {(item, index)=> index.toString()}
           onEndReached ={this.fetchMoreTransactions}
-          onEndReachedThreshold={1}
+          onEndReachedThreshold={0.7}
         /> 
-
-        )
+        </View>
+      );
     }
-}
+  }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 50
+    },
+    searchBar:{
+      flexDirection:'row',
+      height:50,
+      width:'auto',
+      borderWidth:0.5,
+      alignItems:'center',
+      backgroundColor:'grey',
+  
+    },
+    bar:{
+      borderWidth:2,
+      height:50,
+      width:300,
+      paddingLeft:50,
+    },
+    searchButton:{
+      borderWidth:1,
+      height:30,
+      width:50,
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'green'
+    }
+  })
